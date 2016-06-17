0036f7786f1425b5f979bc6cb9faa3121ff995d6
Martin Vizzolini <martinvizzolini@gmail.com>
2016-06-17 02:02:00 -0300

    fixed colours and sizes

--- js/metronome.js
+++ js/metronome.js
@@ -1,14 +1,10 @@
 var audioContext = null;
-var isPlaying = false;      // Are we currently playing?
+var isPlaying = false;      
 var startTime;              // The start time of the entire sequence.
-var currentNote;        // What note is currently last scheduled?
-var lookahead = 25.0;       // How frequently to call scheduling function 
-                            //(in milliseconds)
-var scheduleAheadTime = 1.1;    // How far ahead to schedule audio (sec)
-                            // This is calculated from lookahead, and overlaps 
-                            // with next interval (in case the timer is late)
+var currentNote;            // What note is currently last scheduled?
+var lookahead = 25.0;       // How frequently to call scheduling function (in milliseconds)
+var scheduleAheadTime = 0.1;    // How far ahead to schedule audio (sec)
 var nextNoteTime = 0.0;     // when the next note is due.
-var noteResolution = 0;     // 0 == 16th, 1 == 8th, 2 == quarter note
 var noteLength = 0.04;      // length of "beep" (in seconds)
 var canvas,                 // the canvas element
     canvasContext;          // canvasContext is the canvas' context 2D
@@ -16,8 +12,10 @@ var lastNoteDrawn = -1; // the last "box" we drew on the screen
 var notesInQueue = [];      // the notes that have been put into the web audio,
                             // and may or may not have played yet. {note, time}
 var timerWorker = null;     // The Web Worker used to fire timer messages
+var canvasSize = 200;		// the size of the area where the circle is draw.
+var radius = 70;			// define the size of the circle 
 
-// First, let's shim the requestAnimationFrame API, with a setTimeout fallback
+// set the requestAnimationFrame API
 window.requestAnimFrame = (function(){
     return  window.requestAnimationFrame ||
     window.webkitRequestAnimationFrame ||
@@ -25,17 +23,14 @@ window.requestAnimFrame = (function(){
     window.oRequestAnimationFrame ||
     window.msRequestAnimationFrame ||
     function( callback ){
-        window.setTimeout(callback,30);
+        window.setTimeout(callback,40);
     };
 })();
 
 function nextNote() {
-    // Advance current note and time by a 16th note...
-  //  var secondsPerBeat = 60.0 / tempo;    // Notice this picks up the CURRENT 
-                                          // tempo value to calculate beat length.
     nextNoteTime += 1;    // Add beat length to last beat time
-    currentNote++;    // Advance the beat number, wrap to zero, justo to control the loop
-    if (currentNote == 16) {
+    currentNote++;    // Advance the beat number, wrap to zero, just to control the loop
+    if (currentNote == 2) {
         currentNote = 0;
     }
 }
@@ -105,8 +100,7 @@ function clearCanvas(context, canvas) {
 
 function drawCircle(){
    var centerX = 100;
-   var centerY = 75;
-   var radius = 70;
+   var centerY = 100;
    canvasContext.beginPath();
    canvasContext.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
    canvasContext.fillStyle = 'red';
@@ -126,13 +120,10 @@ function draw() {
 		    var currentTime = audioContext.currentTime;
 		    while (notesInQueue.length && notesInQueue[0].time<currentTime) {
 		        currentNote = notesInQueue[0].note;
-		        c1 = notesInQueue[0].time;
 		        notesInQueue.splice(0,1);   // remove note from queue
-		       
 		    }
 		    // We only need to draw if the note has moved.
 		    if (lastNoteDrawn != currentNote) {
-		    	c1 = audioContext.currentTime;
 		    	drawCircle();
 	            setTimeout(cleanCircle,noteLength*1000);
 		        lastNoteDrawn = currentNote;
@@ -142,13 +133,11 @@ function draw() {
 }
 
 function init(){
-    var container = document.createElement( 'div' );
-    container.className = "container";
+    var container = document.getElementById( 'panel' );
     canvas = document.createElement( 'canvas' );
     canvasContext = canvas.getContext( '2d' );
-    canvas.width = 200; 
-    canvas.height = 200; 
-    document.body.appendChild( container );
+    canvas.width = canvasSize; 
+    canvas.height = canvasSize; 
     container.appendChild(canvas);    
     canvasContext.strokeStyle = "#ffffff";
     canvasContext.lineWidth = 2;
